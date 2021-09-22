d43ceb57f116f994177a85a43c95a3330fac1ddf
liana <liamilky@gmail.com>
2021-09-22 22:31:14 +0300

    Added bye cout

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ int main(int argc, char** argv) {
     for (int i = 0; i < argc; ++i) {
         std::cout << argv[i] << ", ";
     }
-    std::cout << std::endl;
+    std::cout << "bye" << std::endl;
     return 0;
 }
